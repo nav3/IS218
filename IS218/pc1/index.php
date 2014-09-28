77cf5766130c8c80009c24137722a4f00787f612
nav3 <nav3@njit.edu>
2014-09-28 17:05:41 -0400

    Programming Challenge 1 9/28/2014 table working with comments

--- IS218/pc1/index.php
+++ IS218/pc1/index.php
@@ -22,31 +22,31 @@ class filehandling
 		{
     		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) 
     		{
-     			if($column_headings == TRUE) {
+     			if($column_headings == TRUE) 
+     			{
        				$column_heading = $row;
        				$column_headings = FALSE;
      			} else {
        				$record = array_combine($column_heading, $row);
        				$records[] = $record;
-     			}
+     				   }
 
     		}
 
     		fclose($handle);
 	  }
 	  
-	  
 	  if(empty($_GET)) {
-		foreach($records as $record) {
-		  $i++;
-			$college_num = $i - 1;			
-		
-		// Making links to the file
-			 echo '<a href=' . '"http://web.njit.edu/~nav3/IS218/pc1/index.php?record=' . $college_num . '"' . '>College ' . $i . ' </a>';
-			echo '</p>';
-						
-		}
-	}	
+	  	foreach($records as $record) {
+				  $i++;
+				$college_num = $i - 1;			
+			
+			// Making links to the file
+				 echo '<a href=' . '"http://web.njit.edu/~nav3/IS218/pc1/index.php?record=' . $college_num . '"' . '>College ' . $i . ' </a>';
+				echo '</p>';
+					
+			}
+		}	
 	  
 	  $record = $records[$_GET['record']];
 	  // Make the table
@@ -61,9 +61,15 @@ class filehandling
 			//echo '<td> $key . ': ' . $value .  </td>';
 			
 		}
+			
+			
 		echo "</table>";
+	
+	// close of the function 
+	}
+
 }
-	// Creating new obj, and reading the file
+// Creating new obj, and reading the file
 	$newfile = new filehandling();
 	$newfile->readfile_csv("hd2013.csv",TRUE);
 
