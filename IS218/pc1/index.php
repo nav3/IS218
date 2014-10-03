31fea62c74efbccf339906db847ce00800c462b7
nav3 <nav3@njit.edu>
2014-10-03 12:22:05 -0400

    10/3/2014

--- IS218/pc1/index.php
+++ IS218/pc1/index.php
@@ -1,82 +1,109 @@
 <?php
-
 /*
- Nikhil Virparia
+	Nikhil Virparia
 	IS218
-	Challenge 1
+	Assignment 1
 */
-		class File_handling {
-		
-		public static function openFile($file){
-			$file_csv = fopen($file, "r");
-			return $file_csv ;
-		}
-		
-		public static function closeFile($file_csv){
-			fclose($file_csv);
-		}
-		
-	}
 
-	class readcsv extends File_handling{
-		
-		public static $file_csv;
-		public static $colmn_headings;
-		
-		public function readfile_csv($file_csv, $colmn_headings){
+class filehandling 
+{
+	// defining variables
+	public $file_csv;
+	public $column_headings;
+	
+	// Reading csv file
+	public function readfile_csv($file_csv, $column_headings)
+	{
+
+	  ini_set('auto_detect_line_endings',TRUE);
+	  //	variable $file_csv is holding spot for filehandling to be opened
+		if (($handle = fopen($file_csv, "r")) !== FALSE) 
+		{
+    		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+     			if($column_headings == TRUE) {
+       				$column_heading = $row;
+       				$column_headings = FALSE;
+     			} else {
+       				$record = array_combine($column_heading, $row);
+       				$records[] = $record;
+     				   }
+					  // foreach 
+
+    		}
+
+    		fclose($handle);
+	  }
+	  
+	if (empty($_GET)){
 			
-			while(($row = fgetcsv($file_csv, ",")) !== FALSE){
-					if($colmn_headings){
-						$column_heading = $row;
-						$colmn_headings = FALSE;
-					}
-					else{
-						$record = array_combine($column_heading, $row);
-						$records[] = $record;
-					}
-				}
-				return $records;
-		}
-		
-	}
-
-// Print table from csv file
-	class html_Link_table{
-		
-		public static function printTable($records,$url_var){
-			if(isset($_GET[$url_var])){
-				echo '<table border="1">';
-				foreach($records[$_GET[$url_var]] as $key => $value){
-					echo "<tr>";
-					echo "<th> $key </th> <td> &nbsp;&nbsp;&nbsp;$value </td>";
-					echo "</tr>";
-				}
-					echo '</table>';
-			}
-		}
-		
-		// Construct the table 
-			public function __construct($records){
-
 			$i = -1;
-			if(empty($_GET)){
-				foreach($records as $record){
-					$i++;
-					echo '<a href="?record=' .$i. '">' . $record['INSTNM'] . '</a>';
-					echo'</p>';
+
+			foreach($records as $record){
+				$i++;
+				//$University_name= $i -1;
+			// Link Statistic 
+			echo (html_link_table::link("http://web.njit.edu/~nav3/IS218/pc1/index3.php?record=" . $i , $record['INSTNM']));
+				
+				echo '</p>';
 				}
-			}
-			
-			html_Link_table::printTable($records, 'record');
 		}
+	  
+	  // Print out the record inside the link
+	  $record = $records[$_GET['record']];
+	  
+	  echo (html_link_table::table($record));
+
+	
+	// close of the function 
 	}
 	
-$csv = 'hd2013.csv';
-$file = File_handling::openFile($csv);
+}
 
-$file_csv = new readcsv();
-$records = $file_csv->readfile_csv($file, TRUE);
+	
+	  // Make links to the records
+	  class html_link_table {
+	  
+		public static function link($href, $a) {
+		
+		$html = '<a href="' . $href . '">' . $a . '</a>';
+		
+		
+		return $html;
+		
+		}
+		
+		public static function table($record) {
+			  // Make the table
+	  	$html = "<table border=1>";
+      	foreach($record as $key => $value) {
+		$html .= "<tr>";
+			
+		$html .= "<th> $key  </th> <td> &nbsp;&nbsp;&nbsp;$value </td>";
+			$html .= "</tr>";
 
-new html_Link_table($records);
+
+			//echo "<td> $key:</td><td>&nbsp;&nbsp;&nbsp;$value </td>";
+		//	echo "</tr>";
+		}
+		
+			//echo '<td> $key . ': ' . $value .  </td>';
+			
+
+			
+		// close table
+		$html .= "</table>";
+		
+		return $html;
+		}
+	  
+	  }
+	  
+	  
+// Creating new obj, and reading the file
+	$newfile = new filehandling();
+	$newfile2 = new filehandling();
+	
+	$newfile->readfile_csv("hd2013.csv",TRUE);
+	$newfile2->readfile_csv("hd2013_1.csv",TRUE);
 
 ?>
