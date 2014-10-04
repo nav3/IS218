c8f948a3f1ab22437ef26953050784a1031522b2
nav3 <nav3@njit.edu>
2014-10-03 21:29:00 -0400

    10/3/2014 Final Oopy

--- IS218/pc1/index.php
+++ IS218/pc1/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 	Nikhil Virparia
 	IS218
@@ -8,47 +9,54 @@
 class filehandling 
 {
 	// defining variables
-	public $file_csv;
-	public $column_headings;
+	public static $file_csv;
+	public static $column_headings;
 	
 	// Reading csv file
-	public function readfile_csv($file_csv, $column_headings)
+	public static function readfile_csv($file_csv, $column_headings)
 	{
 
 	  ini_set('auto_detect_line_endings',TRUE);
 	  //	variable $file_csv is holding spot for filehandling to be opened
 		if (($handle = fopen($file_csv, "r")) !== FALSE) 
 		{
-    		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-     			if($column_headings == TRUE) {
+    		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) 
+			{
+     			if($column_headings == TRUE) 
+				{
        				$column_heading = $row;
-       				$column_headings = FALSE;
-     			} else {
+       				$column_headings = FALSE; 
+     			} 
+				else 
+				{
        				$record = array_combine($column_heading, $row);
        				$records[] = $record;
-     				   }
-					  // foreach 
+				}
 
     		}
 
     		fclose($handle);
 	  }
+	  return $records; 
+	}
 	  
+
+	  // get the links and table 
+	public function print_table_links($records) 
+	{
 	if (empty($_GET)){
 			
 			$i = -1;
-
 			foreach($records as $record){
 				$i++;
-				//$University_name= $i -1;
-			// Link Statistic 
-			echo (html_link_table::link("http://web.njit.edu/~nav3/IS218/pc1/index3.php?record=" . $i , $record['INSTNM']));
-				
-				echo '</p>';
-				}
+			// Link Statistic could be osl81, or web (both works)
+			echo (html_link_table::link("http://web.njit.edu/~nav3/IS218/pc1/index.php?record=" . $i , $record['INSTNM']));
+			echo '</p>';
 		}
+	}
+
 	  
-	  // Print out the record inside the link
+	  // Print out the table record inside the link
 	  $record = $records[$_GET['record']];
 	  
 	  echo (html_link_table::table($record));
@@ -56,39 +64,32 @@ class filehandling
 	
 	// close of the function 
 	}
-	
-}
-
-	
-	  // Make links to the records
+}	
+	  // Make class for link and table
 	  class html_link_table {
 	  
+		// Function for Link
 		public static function link($href, $a) {
-		
-		$html = '<a href="' . $href . '">' . $a . '</a>';
-		
+			// Creating link
+			$html = '<a href="' . $href . '">' . $a . '</a>';
 		
 		return $html;
 		
 		}
 		
+		// Function for Table
 		public static function table($record) {
-			  // Make the table
-	  	$html = "<table border=1>";
-      	foreach($record as $key => $value) {
-		$html .= "<tr>";
-			
-		$html .= "<th> $key  </th> <td> &nbsp;&nbsp;&nbsp;$value </td>";
+	
+			// Make the table
+			$html = "<table border=1>";
+			foreach($record as $key => $value) 
+			{
+			$html .= "<tr>";
+				
+			$html .= "<th> $key  </th> <td> &nbsp;&nbsp;&nbsp;$value </td>";
 			$html .= "</tr>";
-
-
-			//echo "<td> $key:</td><td>&nbsp;&nbsp;&nbsp;$value </td>";
-		//	echo "</tr>";
 		}
 		
-			//echo '<td> $key . ': ' . $value .  </td>';
-			
-
 			
 		// close table
 		$html .= "</table>";
@@ -98,12 +99,37 @@ class filehandling
 	  
 	  }
 	  
-	  
-// Creating new obj, and reading the file
-	$newfile = new filehandling();
-	$newfile2 = new filehandling();
+	$newfile = filehandling::readfile_csv("hd2013.csv",TRUE);
+	filehandling::print_table_links($newfile);
 	
-	$newfile->readfile_csv("hd2013.csv",TRUE);
-	$newfile2->readfile_csv("hd2013_1.csv",TRUE);
+	// read second file
+	$newfile2 = filehandling::readfile_csv("hd2013_1.csv",TRUE);
+	
+	// passing the second file using static method
+	$new_map = map_new_file::create_map($newfile2);
+
+		//Array combine in map
+					foreach($array23 as $new_map2) {
+					$new_file[$i] = array_combine($new_map, $newfile[$i]);	
+				}
+				
+	// create map
+	class map_new_file{
+		
+		public static function create_map($file_handler) 
+		{
+			
+			foreach($file_handler as $array23) 
+		{
+			$key = $array23['varname'];
+			$value = $array23['varTitle'];
+			$map[$key] = $value;		
+		}
+		return $map;
+		
+		}
+		
+	}
+	
 
 ?>
