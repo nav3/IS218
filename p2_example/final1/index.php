01b0886e2489b360d62e567c3fead8dde6e4c17e
nav3 <nav3@njit.edu>
2014-10-18 23:01:43 -0400

    Final

--- p2_example/final1/index.php
+++ p2_example/final1/index.php
@@ -1,33 +1,35 @@
 <?php
-	//Gives access to functions in other files. 
+	//access to class files
 	require 'Autoloader.php';
 	
-	//Fixes issues with csv files
+	//csv file needs auto detect line endings
 	ini_set('auto_detect_line_endings', TRUE);
 
 	// Change it in one place 
 	use \classes\File as kp;
 	
 	//Stores the csv file in a variable
-	$csv = 'csv/hd2013.csv';
+	$csv1 = 'csv/hd2013.csv';
 	$csv2 = 'csv/hd2013_1.csv';
 
 	// uploading csv file
-	$file = kp\filemanger::uploadFile($csv);
+	$file = kp\filemanger::uploadFile($csv1);
 	$file2 = kp\filemanger::uploadFile($csv2);
 
-	//FileHandling object that passes the file stores into array
+	//Objects stores into array
 	$handle = new kp\handlefile();
 	$handle2 = new kp\handlefile();
 
+		
 	$records = $handle->headings($file, TRUE);
 	$headings = $handle->headings($file2, TRUE);
 
+	//Map 
 	$collegeName = '';
 	if(isset($_GET['record'])){
 		$collegeName = $records[$_GET['record']]['INSTNM'];
 	}
-	/* Print links in table vertically */
+	/* Print tables and link */
 	new \classes\Html\printLinks($records, $headings);
 	\classes\Html\htmlFunctions::printTable($records, $headings);
 	
