ac5b7d2fc1037636b48e5191579c8b31564bcd1d
nav3 <nav3@njit.edu>
2014-10-29 02:19:06 -0400

    twitter

--- IS218/pc3/classs/htmltools.php
+++ IS218/pc3/classs/htmltools.php
@@ -5,16 +5,16 @@
 	{
 		public static function prints(Array $record) {
 			$x = '<h1> <img src="' . $record[0]['user']['profile_image_url'] . '"> ' . $record[0]['user']['name'] . ' Tweets </h1>';
-			$x .= '<h3>' . $record[0]['user']['created_at'] . ' Time and Date of Tweet</h3>';
-			       
+			$x .= '<h3>' . $record[0]['user']['created_at'] . ' Time and Date of Tweet</h3>';     
 			$x .= '<h3>' . $record[0]['user']['followers_count'] . ' Followers</h3>';
 			$x .= '<h3>' . $record[0]['user']['friends_count'] . ' Friends</h3>';
 			$x .= '<h3>' . $record[0]['user']['listed_count'] . ' Listed</h3>';
+			$x .= '<h3>' . $record[0]['user']['screen_name'] . ' Screen name</h3>';
 
 		
 		     //   echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
 			foreach($record as $records) {
-				$x .= '<table border="1"><tr><td>' . $records['text'] . '</td></tr></table>';
+				$x .= '<table border="1" cellpadding="10"><tr><td><p>' . $records['text'] . '</p></td></tr></table>';
 			}
 
 			return $x;
