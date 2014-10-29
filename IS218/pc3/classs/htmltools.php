7726536ed1bda3e20978f06874f41336264707c3
nav3 <nav3@njit.edu>
2014-10-29 02:24:08 -0400

    twitter

--- IS218/pc3/classs/htmltools.php
+++ IS218/pc3/classs/htmltools.php
@@ -4,38 +4,20 @@
 	class htmltools
 	{
 		public static function prints(Array $record) {
+			// print out the image, plus user timeline
 			$x = '<h1> <img src="' . $record[0]['user']['profile_image_url'] . '"> ' . $record[0]['user']['name'] . ' Tweets </h1>';
 			$x .= '<h3>' . $record[0]['user']['created_at'] . ' Time and Date of Tweet</h3>'; 
 			$x .= '<h3>' . $record[0]['user']['screen_name'] . ' Screen name</h3>';			
 			$x .= '<h3>' . $record[0]['user']['followers_count'] . ' Followers</h3>';
 			$x .= '<h3>' . $record[0]['user']['friends_count'] . ' Friends</h3>';
 			$x .= '<h3>' . $record[0]['user']['listed_count'] . ' Listed</h3>';
-
-
-	
+			
+			// Loop the text
 			foreach($record as $records) {
 				$x .= '<table border="1" cellpadding="10"><tr><td><p>' . $records['text'] . '</p></td></tr></table>';
 			}
 
 			return $x;
-		} 
-		
-		public static function Profile(Array $record) {
-			echo '<table border=1>';
-			$y;
-			$index = -1;
-			foreach($record as $records) {
-				$index++;
-				if($index === 0) {
-					foreach($records['user'] as $key => $value) {
-						$y .=  first($key) . $value ; 
-					}
-				}
-			}
-			echo '</table>';
-			$y;
-
-			return $y;
 		}
 
 	}
