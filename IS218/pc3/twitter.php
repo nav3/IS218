bc17f02b46e5158332afc3e9829fda33c77d54d1
nav3 <nav3@njit.edu>
2014-10-28 21:46:29 -0400

    twitter

--- IS218/pc3/twitter.php
+++ IS218/pc3/twitter.php
@@ -1,6 +1,18 @@
 <?php
+require_once('TwitterAPIExchange.php');
 //Twitter API
 echo "<h2>Simple Twitter API Test</h2>";
+
+
+ 
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "244220520-5Zqsbyg0H16PhutEcPsIJclYPTkvLYwCI4E1dsst",
+    'oauth_access_token_secret' => "Y9KTxUjYQcvIdLitIARHcNEdT5EBPNefuCUZpgg8Hbm11",
+    'consumer_key' => "26ByypCHIDoaNnAxkljOkDcNw",
+    'consumer_secret' => "nkYu1ogLV3tSwAfxTjGvuX7vfmytWJceFMoXAps4A0RfVPwVxA"
+);
+
 
 
 ?>
