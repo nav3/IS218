e0d3d8fc948db22a6568836a6e61a2dde90bfa4f
nav3 <nav3@njit.edu>
2014-10-29 02:09:44 -0400

    twitter

--- IS218/pc3/classs/htmltools.php
+++ IS218/pc3/classs/htmltools.php
@@ -4,18 +4,22 @@
 	class htmltools
 	{
 		public static function prints(Array $record) {
-			$x = '<img src="' . $record[0]['user']['profile_image_url'] . '">';
+			$x = '<h1>' . $record[0]['user']['name'] . ' Tweets <img src="' . $record[0]['user']['profile_image_url'] . '"> </h1>';
+			//$x .= '<img src="' . $record[0]['user']['profile_image_url'] . '"> ';
+			$x .= '<h3>' . $record[0]['user']['created_at'] . ' Time and Date of Tweet</h3>';
+			       
 			$x .= '<h3>' . $record[0]['user']['followers_count'] . ' Followers</h3>';
 			$x .= '<h3>' . $record[0]['user']['friends_count'] . ' Friends</h3>';
 			$x .= '<h3>' . $record[0]['user']['listed_count'] . ' Listed</h3>';
-			$x .= '<h1>' . $record[0]['user']['name'] . ' Tweets</h1>';
+
 		
+		     //   echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
 			foreach($record as $records) {
-				$x .= '<div>' . $records['text'] . '</div>';
+				$x .= '<table border="1"><tr><td>' . $records['text'] . '</td></tr></table>';
 			}
 
 			return $x;
-		} //end function
+		} 
 		
 		public static function Profile(Array $record) {
 			echo '<table border=1>';
@@ -25,7 +29,7 @@
 				$index++;
 				if($index === 0) {
 					foreach($records['user'] as $key => $value) {
-						$y .=  ucfirst($key) . $value ; 
+						$y .=  first($key) . $value ; 
 					}
 				}
 			}
@@ -33,8 +37,7 @@
 			$y;
 
 			return $y;
-		} //end function
-
+		}
 
 	}
 ?>
