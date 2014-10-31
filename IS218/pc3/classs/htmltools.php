2bff677b5ec70f626dfb928bc96c9101f66a4e72
nav3 <nav3@njit.edu>
2014-10-30 23:25:21 -0400

    twiiter

--- IS218/pc3/classs/htmltools.php
+++ IS218/pc3/classs/htmltools.php
@@ -2,18 +2,20 @@
 
 namespace classs;
 	class htmltools{
-
-		public static $timeline_labels = ['Time and Date of Tweet','Tweet','Tweeted By','Screen name','Followers','Friends'];	
+		//public static $Hometimeline_labels = ['Time and Date of Tweet','Tweet','Tweeted By','Screen name','Followers','Friends'];
+		public static $post_labels = ['Posted On','Post','Screen Name'];
+		public static $timelables = ['Time and Date of Tweet','Tweet','Tweeted By','Screen name','Followers','Friends'];	
+		public static $fblabels = ['Name','Screen name'];
 		
 		//Printing Timeline
-		public static function prTimeline($getfield, $string){
+		public static function timeline($field, $string){
 			$x = '<h1> <img src="' . $string[0]['user']['profile_image_url'] . '"> ' . $string[0]['user']['name'] . ' Tweets</td></tr> </h1>';
 
 				echo '<table><tr>';
 				
 				$i = 0;
-				foreach(self::$timeline_labels as $label){					
-					echo '<th>' . self::$timeline_labels[$i] . '</th>';
+				foreach(self::$timelables as $label){					
+					echo '<th>' . self::$timelables[$i] . '</th>';
 					$i++;
 				}
 					foreach($string as $items){					
@@ -28,9 +30,29 @@ namespace classs;
 				}			
 					$tbl .= '</table>';
 					echo $tbl;
-							
-	
+						
 		}
+		
+		// follower
+		public static function follower($string){
+				echo '<table><tr>';
+				$i = 0;
+				foreach(self::$fblabels as $fblabels){					
+					echo '<th>' . self::$fblabels[$i] . '</th>';
+					$i++;
+				}
+					foreach($string[0] as $items){					
+					$tbl .= '<tr>';
+					$tbl .= '<td>' . $items['name'] .'</td>';
+					$tbl .= '<td>' . $items['screen_name'] . '</td>';
+					$tbl .= '</tr>';
+				}			
+					$tbl .= '</table>';
+					echo $tbl; 
+				
+			}
+		
+		
 	
 
 		}
