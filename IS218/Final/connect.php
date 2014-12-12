02df64b476690dee174c05a9e99339559b7ef89b
nav3 <nav3@njit.edu>
2014-12-12 01:38:38 -0500

    Final

---
+++ IS218/Final/connect.php
@@ -0,0 +1,8 @@
+<?php
+	try {
+		$dbh = new PDO('mysql:host=localhost; dbname=college_data; charset=utf8', 'root', 'cadcszxcadc12');
+	}catch(Exception $e) {
+		print "Error: " . $e->getMessage() . "<br>";
+		die();
+	}	
+?>
